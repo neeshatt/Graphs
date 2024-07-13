9dd5df25884b9ee6e0f31b22075b154c9476e968
rubaid <rubaidahmed04@gmail.com>
2024-07-13 22:46:25 +0600

    a

--- homepage.js
+++ homepage.js
@@ -1,83 +1,83 @@
 const getTreeData = () => {
-  return {
-    element: 'A',
+return {
+  element: 'A',
+  left: {
+    element: 'B',
     left: {
-      element: 'B',
+      element: 'B1',
+    },
+    right: {
+      element: 'B2',
+    },
+  },
+  right: {
+    element: 'C',
+    left: {
+      element: 'C1',
       left: {
-        element: 'B1',
-      },
-      right: {
-        element: 'B2',
+        element: 'C11',
+        left: {
+          element: 'C12',
+        },
       },
     },
     right: {
-      element: 'C',
+      element: 'C2',
       left: {
-        element: 'C1',
-        left: {
-          element: 'C11',
-          left: {
-            element: 'C12',
-          },
-        },
+        element: 'C21',
       },
       right: {
-        element: 'C2',
+        element: 'C22',
         left: {
-          element: 'C21',
+          element: 'C221',
+          left: {
+            element: 'C2211',
+          },
         },
         right: {
-          element: 'C22',
-          left: {
-            element: 'C221',
-            left: {
-              element: 'C2211',
-            },
-          },
+          element: 'C222',
           right: {
-            element: 'C222',
-            right: {
-              element: 'C2221',
-            },
+            element: 'C2221',
           },
         },
       },
     },
-  };
+  },
+};
 };
 
 export const renderBinaryTree = (node) => {
-  const { left, right, element} = node;
-  return `
-    <div class="node__element">${element}</div>
-    ${
-      left || right
-        ? `
-          <div class="node__bottom-line"></div>
-          <div class="node__children">
-            ${
-              left
-                ? `
-                <div class="node node--left">
-                  ${renderBinaryTree(left)}
-                </div>
-                `
-                : ''
-            }
-            ${
-              right
-                ? `
-                <div class="node node--right">
-                  ${renderBinaryTree(right)}
-                </div>
-                `
-                : ''
-            }
-          </div>
-        `
-        : ''
-    }
-  `;
+const { left, right, element} = node;
+return `
+  <div class="node__element">${element}</div>
+  ${
+    left || right
+      ? `
+        <div class="node__bottom-line"></div>
+        <div class="node__children">
+          ${
+            left
+              ? `
+              <div class="node node--left">
+                ${renderBinaryTree(left)}
+              </div>
+              `
+              : ''
+          }
+          ${
+            right
+              ? `
+              <div class="node node--right">
+                ${renderBinaryTree(right)}
+              </div>
+              `
+              : ''
+          }
+        </div>
+      `
+      : ''
+  }
+`;
 };
 
 const main = () => {
